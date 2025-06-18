1ac7496808fd6d2e258203e02d8101a3050a97c6
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-19 00:12:42 +0600

    Java : Print Variables

--- Java_W3School/_1_Java_Tutorial/_7_Variables/_2_Print_Variable.java
+++ Java_W3School/_1_Java_Tutorial/_7_Variables/_2_Print_Variable.java
@@ -1,10 +1,10 @@
 /*
 Java : Print Variables
 */
-
+ 
 package Java_W3School._1_Java_Tutorial._7_Variables;
 
-public class Print_Variable {
+public class _2_Print_Variable {
     public static void main(String[] args) {
         String name = "Simanto";
         int roll = 4;
