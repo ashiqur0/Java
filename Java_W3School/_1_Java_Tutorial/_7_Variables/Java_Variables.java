521f148aef53530a62b87577e028f92697a06ede
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-18 23:48:41 +0600

    Java : Java Variables

---
+++ Java_W3School/_1_Java_Tutorial/_7_Variables/Java_Variables.java
@@ -0,0 +1,25 @@
+/*
+Java : Java Variables
+*/
+package Java_W3School._1_Java_Tutorial._7_Variables;
+
+public class Java_Variables {
+    public static void main(String[] args) {
+        // Declaring and Creating Variables
+        String name = "Samiul";
+        int roll = 1;
+        float gpa = 3.88f;
+        char grade = 'A';
+        boolean isPassed = true;
+
+        // Assign new value to existign variable 
+        name = "Nawshin";
+        roll = 2;
+        gpa = 3.78f;
+        grade = 'A';
+        isPassed = false;
+
+        // Final Variables : the value is constant (unchangable)
+        final int marks = 98;
+    }
+}
