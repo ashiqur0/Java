8a45150e9ab3d93d16499468b40634733d5f25b1
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-18 20:08:06 +0600

    Java : First Program

---
+++ Java_Tutorial/Java_Home/Main.java
@@ -0,0 +1,10 @@
+/*
+ * Java : First Program
+ */
+
+package Java_Tutorial.Java_Home;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
