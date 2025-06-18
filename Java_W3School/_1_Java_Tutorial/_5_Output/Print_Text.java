3d5a405fbfa8d6c51b15e6f322c787707c220db0
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-18 23:14:05 +0600

    Java : Print Text

---
+++ Java_W3School/_1_Java_Tutorial/_5_Output/Print_Text.java
@@ -0,0 +1,13 @@
+/*
+Java : Print Text
+*/
+
+package Java_W3School._1_Java_Tutorial._5_Output;
+
+public class Print_Text {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I'm Learning Java");
+        System.out.println("It's awesome");
+    }
+}
