812eeadf61fcd70746c22aae3d0ebad120349344
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-18 23:20:35 +0600

    Java : Print Text

--- Java_W3School/_1_Java_Tutorial/_5_Output/Print_Text.java
+++ Java_W3School/_1_Java_Tutorial/_5_Output/Print_Text.java
@@ -9,5 +9,13 @@ public class Print_Text {
         System.out.println("Hello World!");
         System.out.println("I'm Learning Java");
         System.out.println("It's awesome");
+        
+        // Double quotes : Text must be wrapped inside the double quotation marks ""
+        System.out.println("I'm Learning Java");    // execute successfully
+        // System.out.println(I'm Learning Java);     // error
+
+        // print() method : print without line brake
+        System.out.print("Hello World!");
+        System.out.print("I'll print on the same line.");
     }
 }
