cdf8eff9c3025a7104a73c5b8d08c8e12ccca8cc
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-18 22:49:29 +0600

    Java : First Program

--- Java_W3School/_1_Java_Tutorial/_1_Java_Home/Main.java
+++ Java_W3School/_1_Java_Tutorial/_1_Java_Home/Main.java
@@ -2,7 +2,7 @@
  * Java : First Program
  */
 
-package Java_Tutorial.Java_Home;
+package Java_W3School._1_Java_Tutorial._1_Java_Home;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
