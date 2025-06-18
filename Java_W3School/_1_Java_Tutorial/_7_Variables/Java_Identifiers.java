628c34ba5ed7c4b6fbcc346ef923d5a8d1957361
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-19 00:07:40 +0600

    Java : Java Identifiers

---
+++ Java_W3School/_1_Java_Tutorial/_7_Variables/Java_Identifiers.java
@@ -0,0 +1,25 @@
+/*
+Java : Java Identifiers
+Identifiers : all variable name must be identified with unique name. These unique name is called identifiers
+Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
+Note: It is recommended to use descriptive names in order to create understandable and maintainable code:
+
+The general rules for naming variables are: (collected from w3school's Java tutorial)
+Names can contain letters, digits, underscores, and dollar signs
+Names must begin with a letter
+Names should start with a lowercase letter, and cannot contain whitespace
+Names can also begin with $ and _
+Names are case-sensitive ("myVar" and "myvar" are different variables)
+Reserved words (like Java keywords, such as int or boolean) cannot be used as names
+*/
+
+package Java_W3School._1_Java_Tutorial._7_Variables;
+
+public class Java_Identifiers {
+    public static void main(String[] args) {
+        int age = 25;
+        int sum = 20;
+        int minutePerHour = 60;
+        System.out.println(age + " "+ sum + " " + minutePerHour);
+    }
+}
