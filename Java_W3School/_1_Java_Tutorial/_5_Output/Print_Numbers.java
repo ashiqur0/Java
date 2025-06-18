b88af2e3e58e8e3fe47ebb2cbed620781027d709
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-18 23:23:53 +0600

    Java : Print Numbers

---
+++ Java_W3School/_1_Java_Tutorial/_5_Output/Print_Numbers.java
@@ -0,0 +1,19 @@
+/*
+Java : Print Numbers
+*/
+
+package Java_W3School._1_Java_Tutorial._5_Output;
+
+public class Print_Numbers {
+    public static void main(String[] args) {
+
+        // print numbes
+        System.out.println(123);
+        System.out.println(100);
+        System.out.println(15000);
+
+        // perform mathmatical operation
+        System.out.println(6 * 3);
+        System.out.println(6 + 3);
+    }
+}
