98e4c072b1089e1023ef03b2c1a93b171c770a1f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-19 09:01:06 +0600

    Java : Non Primitive Data Types

---
+++ Java_W3School/_1_Java_Tutorial/_8_Data_Types/_7_Non_Primitive_Type.java
@@ -0,0 +1,17 @@
+/*
+Java : Non Primitive Data Types
+Non Primitive Data Types : are called reference type because the refer to objects
+
+Features : (studied from w3school)
+1. Primitive types in Java are predefined and built into the language, while non-primitive types are created by the programmer (except for String).
+2. Non-primitive types can be used to call methods to perform certain operations, whereas primitive types cannot.
+3. Primitive types start with a lowercase letter (like int), while non-primitive types typically starts with an uppercase letter (like String).
+*/
+
+package Java_W3School._1_Java_Tutorial._8_Data_Types;
+
+public class _7_Non_Primitive_Type {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
