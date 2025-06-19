cb9fdba4e82930ef910d0c503f6edb6e696a3d10
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-19 19:47:51 +0600

    String Reference : replace() method

---
+++ Java_W3School/_1_Java_Tutorial/_11_Strings/String_Reference/_14_replace.java
@@ -0,0 +1,15 @@
+/*
+String Reference : replace() method
+replace() : replace a character to another
+*/
+
+package Java_W3School._1_Java_Tutorial._11_Strings.String_Reference;
+
+public class _14_replace {
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println(str.replace('l', 'p'));
+        System.out.println(str);
+        System.out.println(str.replace("ll", "pp"));
+    }
+}
