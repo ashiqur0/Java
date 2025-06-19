3da54a8f55a7c4d15769f9882824b55665c7bb72
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-19 16:30:19 +0600

    String Reference : endsWith() method

---
+++ Java_W3School/_1_Java_Tutorial/_11_Strings/String_Reference/_4_endsWith.java
@@ -0,0 +1,15 @@
+
+/*
+String Reference : endsWith() method
+endsWith() : check if String ends with a sequence of character
+*/
+package Java_W3School._1_Java_Tutorial._11_Strings.String_Reference;
+
+public class _4_endsWith {
+    public static void main(String[] args) {
+        String txt = "Hello";
+        System.out.println(txt.endsWith("llo"));  // true
+        System.out.println(txt.endsWith("o"));    // true
+        System.out.println(txt.endsWith("hi"));   // false
+    }
+}
