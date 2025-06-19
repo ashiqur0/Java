835f65ba4b081f3838d736fbb435b303c73e05b9
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-19 20:08:14 +0600

    String Reference : split() method

---
+++ Java_W3School/_1_Java_Tutorial/_11_Strings/String_Reference/_15_split.java
@@ -0,0 +1,18 @@
+/*
+String Reference : split() method
+split() : Split a string into an array of strings:
+*/
+
+
+package Java_W3School._1_Java_Tutorial._11_Strings.String_Reference;
+
+public class _15_split {
+    public static void main(String[] args) {
+        String myStr = "Split a string by spaces, and also puntuation.";
+        String regex = "[,\\.\\s]";
+        String[] myStrings = myStr.split(regex);
+        for (int i = 0; i < myStrings.length; i++) {
+            System.out.println(myStrings[i]);
+        }
+    }
+}
