63ebc40f799f215ff1987498c4f661c3cbd39800
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-19 08:10:30 +0600

    Java : Data Types

---
+++ Java_W3School/_1_Java_Tutorial/_8_Data_Types/_1_Data_Types.java
@@ -0,0 +1,29 @@
+/*
+Java : Data Types
+Primitive Data Types : boolean, byte, char, short, int, long, float, double
+Non-primitive Data Types : String, arrays, classes, interfaces
+
+// Studied from w3 School
+boolean : Stores true or false values
+byte : Stores whole numbers from -128 to 127
+char : Stores a single character/letter or ASCII values
+short : Stores whole numbers from -32,768 to 32,767
+int : Stores whole numbers from -2,147,483,648 to 2,147,483,647
+long : Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+float : Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+double : Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits
+*/
+
+package Java_W3School._1_Java_Tutorial._8_Data_Types;
+
+public class _1_Data_Types {
+    public static void main(String[] args) {
+        int myNum = 98;             // Integer (whole number)
+        float myFloatNum = 5.99f;   // Floatint Point Number
+        char myLetter = 'D';        // Character
+        boolean myBool = true;      // Boolean
+        String myString = "Hello";  // String 
+
+        System.out.println(myNum + " " + myFloatNum + " " + myLetter + " " + myBool + " " + myString);
+    }
+}
