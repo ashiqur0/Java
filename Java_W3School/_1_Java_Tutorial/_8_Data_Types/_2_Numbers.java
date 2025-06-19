bbc49f9810eedededb1c630385a4de067f5bc363
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-19 08:26:59 +0600

    Java : Numbers

---
+++ Java_W3School/_1_Java_Tutorial/_8_Data_Types/_2_Numbers.java
@@ -0,0 +1,38 @@
+/*
+Java : Numbers
+Primitive number types are divided into two groups : Integer types and Floating point types 
+Integer types : byte, short, int, long
+Floating point types : float, double
+*/
+
+package Java_W3School._1_Java_Tutorial._8_Data_Types;
+
+public class _2_Numbers {
+    public static void main(String[] args) {
+        // Integer Type : 
+        // byte : Stores whole numbers from -128 to 127
+        byte myByteNum = 100;
+        System.out.println(myByteNum);
+
+        // short : Stores whole numbers from -32,768 to 32,767
+        short myShortNum = 5000;
+        System.out.println(myShortNum);
+
+        // int : Stores whole numbers from -2,147,483,648 to 2,147,483,647
+        int myIntNum = 1000000000;
+        System.out.println(myIntNum);
+
+        // long : Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        long myLongNum = 1000000000;
+        System.out.println(myLongNum);
+
+        // Floating point types : 
+        // float : float : Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+        float myFloatNum = 2.99f;
+        System.out.println(myFloatNum);
+
+        // double : Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits
+        double myDoubleNum = 3.141590000001;
+        System.out.println(myDoubleNum);
+    }
+}
