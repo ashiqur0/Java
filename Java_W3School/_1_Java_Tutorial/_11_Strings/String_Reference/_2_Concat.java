358b5ac2b88b1d5f84ac1d4f2981ba47bc684825
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-19 16:20:13 +0600

    String Reference : concat() method

--- Java_W3School/_1_Java_Tutorial/_11_Strings/String_Reference/_2_Concat.java
+++ Java_W3School/_1_Java_Tutorial/_11_Strings/String_Reference/_2_Concat.java
@@ -5,7 +5,7 @@ concat() : appends a string to end of another string
 
 package Java_W3School._1_Java_Tutorial._11_Strings.String_Reference;
 
-public class _2_Concat {
+public class _2_concat {
     public static void main(String[] args) {
         String first = "Ashiqur ";
         String last =  "Rahman";
---
+++ Java_W3School/_1_Java_Tutorial/_11_Strings/String_Reference/_3_contains.java
@@ -0,0 +1,5 @@
+package Java_W3School._1_Java_Tutorial._11_Strings.String_Reference;
+
+public class _3_contains {
+    
+}
