83a3be05df4ea613360b791c54f62e4e1ff65e56
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-19 17:00:45 +0600

    String Reference : indexOf() method

---
+++ Java_W3School/_1_Java_Tutorial/_11_Strings/String_Reference/_9_indexOf.java
@@ -0,0 +1,13 @@
+/*
+String Reference : indexOf() method
+indexOf() : Search a string for the first occurrence of searched character of sequence of character
+*/
+
+package Java_W3School._1_Java_Tutorial._11_Strings.String_Reference;
+
+public class _9_indexOf {
+    public static void main(String[] args) {
+        String str = "Hello, Java Programmar";
+        System.out.println(str.indexOf('a'));
+    }
+}
