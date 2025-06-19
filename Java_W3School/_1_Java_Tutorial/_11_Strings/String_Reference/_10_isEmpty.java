d0948c3323cea6f39dff66f0e811924fa4fca7b7
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-19 17:05:52 +0600

    String Reference : isEmpty() method

--- Java_W3School/_1_Java_Tutorial/_11_Strings/String_Reference/_10_isEmpty.java
+++ Java_W3School/_1_Java_Tutorial/_11_Strings/String_Reference/_10_isEmpty.java
@@ -3,7 +3,6 @@ String Reference : isEmpty() method
 isEmpty() : check if a string or array is empty or not
 */
 
-
 package Java_W3School._1_Java_Tutorial._11_Strings.String_Reference;
 
 public class _10_isEmpty {
