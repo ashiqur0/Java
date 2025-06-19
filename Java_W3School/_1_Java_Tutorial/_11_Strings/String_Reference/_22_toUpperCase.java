0af366567669ae8e1b54a6e45ede199ac6bed25d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-19 20:44:41 +0600

    String Reference : toUpperCase() method

---
+++ Java_W3School/_1_Java_Tutorial/_11_Strings/String_Reference/_22_toUpperCase.java
@@ -0,0 +1,13 @@
+/*
+String Reference : toUpperCase() method
+toUpperCase() : convert a strign to uppercase
+*/
+
+package Java_W3School._1_Java_Tutorial._11_Strings.String_Reference;
+
+public class _22_toUpperCase {
+    public static void main(String[] args) {
+        String str = "hello";
+        System.out.println(str.toUpperCase());
+    }
+}
