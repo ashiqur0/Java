a6e4985fb905415f3d984ebd49e0301e17886e8a
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-19 20:41:20 +0600

    String Reference : toString() method

---
+++ Java_W3School/_1_Java_Tutorial/_11_Strings/String_Reference/_21_toString.java
@@ -0,0 +1,13 @@
+/*
+String Reference : toString() method
+toString() : returns the string itself. (কিছুই বুঝলাম না)
+*/
+
+package Java_W3School._1_Java_Tutorial._11_Strings.String_Reference;
+
+public class _21_toString {
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println(str.toString());
+    }
+}
