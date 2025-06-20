dae2ee97521fd09f3f680e8204668b635c9bdc03
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-20 08:22:44 +0600

    Java Math : Math.sqrt() method

--- Java_W3School/_1_Java_Tutorial/_12_Math/_3_sqrt.java
+++ Java_W3School/_1_Java_Tutorial/_12_Math/_3_sqrt.java
@@ -1,13 +1,10 @@
 /*
 Java Math : Math.sqrt() method
-Math.sqrt() : returns the squareroot values
+Math.sqrt() : returns the squareroot double values
 */
 
 public class _3_sqrt {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 8;
-        int min_value = Math.sqrt(64.0);
-        System.out.println("Max Value : " + min_value);  // 5
+        System.out.println(Math.sqrt(64));  // 8
     }
 }
