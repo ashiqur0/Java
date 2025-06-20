6e6b0b53bd36445b4456f272100b6b1bf450e01b
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-20 08:15:18 +0600

    Java Math : Math.max() method

---
+++ Java_W3School/_1_Java_Tutorial/_12_Math/_1_max.java
@@ -0,0 +1,13 @@
+/*
+Java Math : Math.max() method
+Math.max() : returns the greater of two value
+*/
+
+public class _1_max {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 13;
+        int max_value = Math.max(num1, num2);
+        System.out.println("Max Value :" + max_value);  // 13
+    }
+}
