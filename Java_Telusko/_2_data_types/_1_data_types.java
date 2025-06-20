b70a27053412df2cb89071bff744588948e84c50
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-20 12:32:42 +0600

    Java : Data Types

---
+++ Java_Telusko/_2_data_types/_1_data_types.java
@@ -0,0 +1,38 @@
+/*
+Java : Data Types
+byte byteNum    : 1 byte
+short shortNum  : 2 byte
+int intNum      : 4 byte
+long longNum    : 8 byte
+
+float floatNum  : 4 byte
+double doubleNum: 8 byte
+
+char charNum    : 2 byte
+boolean bool    : 1 byte
+*/
+
+package Java_Telusko._2_data_types;
+
+public class _1_data_types {
+    public static void main(String[] args) {
+        byte byteNum = 127;
+        short shortNum = 32665;
+        int intNum = 1013475938;
+        long longNum = 1234567891010121314l;
+        System.out.println(byteNum);
+        System.out.println(shortNum);
+        System.out.println(intNum);
+        System.out.println(longNum);
+
+        float floatNum = 3.14f;
+        double doubleNum = 3.14;
+        System.out.println(floatNum);
+        System.out.println(doubleNum);
+
+        char charNum = 65;
+        boolean bool = true;
+        System.out.println(charNum);
+        System.out.println(bool);        
+    }
+}
