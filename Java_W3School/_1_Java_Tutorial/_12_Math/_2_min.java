1a373cd8af63f199fce61ba5bc78c9e0a414fb97
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-20 08:21:08 +0600

    Java Math : Math.min() method

--- Java_W3School/_1_Java_Tutorial/_12_Math/_2_min.java
+++ Java_W3School/_1_Java_Tutorial/_12_Math/_2_min.java
@@ -8,6 +8,6 @@ public class _2_min {
         int num1 = 5;
         int num2 = 8;
         int min_value = Math.min(num1, num2);
-        System.out.println("Max Value : " + min_value);  // 5
+        System.out.println("Min Value : " + min_value);  // 5
     }
 }
---
+++ Java_W3School/_1_Java_Tutorial/_12_Math/_3_sqrt.java
@@ -0,0 +1,13 @@
+/*
+Java Math : Math.sqrt() method
+Math.sqrt() : returns the squareroot values
+*/
+
+public class _3_sqrt {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 8;
+        int min_value = Math.sqrt(64.0);
+        System.out.println("Max Value : " + min_value);  // 5
+    }
+}
