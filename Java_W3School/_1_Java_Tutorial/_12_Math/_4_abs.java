bb0d36dcc2d800dd712aac1be2ec97f0bfecab45
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-20 08:25:46 +0600

    Java Math : Math.abs() method

---
+++ Java_W3School/_1_Java_Tutorial/_12_Math/_4_abs.java
@@ -0,0 +1,10 @@
+/*
+Java Math : Math.abs() method
+Math.abs() : returns absolute value (positive)
+*/
+
+public class _4_abs {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-4 + 1));  // 3
+    }
+}
