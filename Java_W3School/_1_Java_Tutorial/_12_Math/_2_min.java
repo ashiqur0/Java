18e63217563c2bae185560a0ab1267accb62bf44
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-20 08:17:44 +0600

    Java Math : Math.min() method

---
+++ Java_W3School/_1_Java_Tutorial/_12_Math/_2_min.java
@@ -0,0 +1,13 @@
+/*
+Java Math : Math.min() method
+Math.min() : returns the smaller of two values
+*/
+
+public class _2_min {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 8;
+        int min_value = Math.min(num1, num2);
+        System.out.println("Max Value : " + min_value);  // 5
+    }
+}
