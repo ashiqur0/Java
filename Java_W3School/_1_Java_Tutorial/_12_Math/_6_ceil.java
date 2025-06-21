ab5852e0399604abe71ae768079da9a3fb27dd19
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-22 00:10:32 +0600

    Java Math : Math.ceil() method

---
+++ Java_W3School/_1_Java_Tutorial/_12_Math/_6_ceil.java
@@ -0,0 +1,10 @@
+/*
+Java Math : Math.ceil() method
+Math.ceil() : returns double type rounded up value
+*/
+public class _6_ceil {
+    public static void main(String[] args) {
+        System.out.println(Math.ceil(0.6)); // 1.0
+        System.out.println(Math.ceil(0.1)); // 1.0
+    }
+}
