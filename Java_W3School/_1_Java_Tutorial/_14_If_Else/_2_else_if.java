9ecea2129fb89d9aa3186b145146246e2067a681
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-22 07:01:38 +0600

    Java if-else : Java condition and if else statement

---
+++ Java_W3School/_1_Java_Tutorial/_14_If_Else/_2_else_if.java
@@ -0,0 +1,18 @@
+/*
+Java if-else : Java condition and if else statement
+if-else: execute first operation if the statement is true, 2nd otherwise
+*/
+
+package Java_W3School._1_Java_Tutorial._14_If_Else;
+
+public class _2_else_if {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 5;
+        if (x < y) {
+            System.out.println("x is smaller");
+        } else {
+            System.out.println("y is smaller");
+        }
+    }
+}
