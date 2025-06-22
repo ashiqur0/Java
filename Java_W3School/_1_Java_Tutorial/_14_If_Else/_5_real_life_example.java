84fe1dff558c4cbe92034b36fd454b217500b8ab
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-22 07:14:53 +0600

    Java if-else : real life example

---
+++ Java_W3School/_1_Java_Tutorial/_14_If_Else/_5_real_life_example.java
@@ -0,0 +1,18 @@
+/*
+Java if-else : real life example
+*/
+
+package Java_W3School._1_Java_Tutorial._14_If_Else;
+
+public class _5_real_life_example {
+    public static void main(String[] args) {
+        int num = 8;
+        if (num > 0) {
+            System.out.println("Num is positive");
+        } else if (num < 0) {
+            System.out.println("Num is negative");
+        } else {
+            System.out.println("Num is 0");
+        }
+    }
+}
