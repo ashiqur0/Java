c13bfbfd693de0e9e3564c9148399cfc6d361b1e
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-22 08:09:29 +0600

    Java Array : Array

--- Java_W3School/_1_Java_Tutorial/_19_Arrays/_1_array.java
+++ Java_W3School/_1_Java_Tutorial/_19_Arrays/_1_array.java
@@ -6,9 +6,25 @@ package Java_W3School._1_Java_Tutorial._19_Arrays;
 
 public class _1_array {
     public static void main(String[] args) {
+
+        // Array : integer type
         int[] arr = {10, 20, 30, 40};
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        // Array : char type
+        char[] ch = {'A', 'B', 'C', 'D'};
+        for (int i = 0; i < ch.length; i++) {
+            System.out.print(ch[i] + " ");
+        }
+        System.out.println();
+
+        // Array : String type
+        String[] fruits = {"apple", "banana", "cherry", "kiwi"};
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.print(fruits[i] + " ");
         }
     }
 }
