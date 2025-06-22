6411a3e09c281729a6df67d40dda5deff56143ec
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-22 06:22:59 +0600

    Java Math : Math.floor() method

---
+++ Java_W3School/_1_Java_Tutorial/_12_Math/_7_floor.java
@@ -0,0 +1,12 @@
+/*
+Java Math : Math.floor() method
+Math.floor() : Round numbers down to the nearest integer
+*/
+
+public class _7_floor {
+    public static void main(String[] args) {
+        System.out.println(Math.floor(0.8));   // 0.0
+        System.out.println(Math.floor(5.1));   // 5.0
+        System.out.println(Math.floor(-6.3));   // -7.0
+    }
+}
