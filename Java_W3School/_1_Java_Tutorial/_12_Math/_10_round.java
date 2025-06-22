75db54a4834b967c316acd660e8db5ec99b5fe9a
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-22 06:39:21 +0600

    Java Math : Math.rint() method

---
+++ Java_W3School/_1_Java_Tutorial/_12_Math/_10_round.java
@@ -0,0 +1,13 @@
+/*
+Java Math : Math.round() method
+Math.round() : round the number to the nearest integer.
+*/
+
+public class _10_round {
+    public static void main(String[] args) {
+        System.out.println(Math.round(0.5)); // 1
+        System.out.println(Math.round(1.5)); // 2
+        System.out.println(Math.round(-5.1)); // -5
+        System.out.println(Math.round(-5.9)); // -6
+    }
+}
--- Java_W3School/_1_Java_Tutorial/_12_Math/_9_rint.java
+++ Java_W3School/_1_Java_Tutorial/_12_Math/_9_rint.java
@@ -1,13 +1,13 @@
 /*
 Java Math : Math.rint() method
-Math.rint() : round the number to the nearest integer.
+Math.rint() : round the number to the nearest integer of double type.
 */
 
 public class _9_rint {
     public static void main(String[] args) {
-        System.out.println(Math.rint(0.5)); // 0
-        System.out.println(Math.rint(1.5)); // 2
-        System.out.println(Math.rint(-5.1)); // -5
-        System.out.println(Math.rint(-5.9)); // -6
+        System.out.println(Math.rint(0.5)); // 0.0
+        System.out.println(Math.rint(1.5)); // 2.0
+        System.out.println(Math.rint(-5.1)); // -5.0
+        System.out.println(Math.rint(-5.9)); // -6.0
     }
 }
