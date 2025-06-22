e7460bae318aef991966a608dbb3b2d0cf55da22
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-22 06:30:11 +0600

    Java Math : Math.pow() method

---
+++ Java_W3School/_1_Java_Tutorial/_12_Math/_8_pow.java
@@ -0,0 +1,10 @@
+/*
+Java Math : Math.pow() method
+Math.pow() : raises a number to the power of another number.
+*/
+
+public class _8_pow {
+    public static void main(String[] args) {
+        System.out.println(Math.pow(2, 3)); // 8.0
+    }
+}
