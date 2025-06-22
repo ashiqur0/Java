40dabe73684c8fe865b3055276d352c0c0b512cd
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-22 06:34:14 +0600

    Java Math : Math.rint() method

---
+++ Java_W3School/_1_Java_Tutorial/_12_Math/_9_rint.java
@@ -0,0 +1,13 @@
+/*
+Java Math : Math.rint() method
+Math.rint() : round the number to the nearest integer.
+*/
+
+public class _9_rint {
+    public static void main(String[] args) {
+        System.out.println(Math.rint(0.5)); // 0.0
+        System.out.println(Math.rint(1.5)); // 2.0
+        System.out.println(Math.rint(-5.1)); // -5.0
+        System.out.println(Math.rint(-5.9)); // -6.0
+    }
+}
