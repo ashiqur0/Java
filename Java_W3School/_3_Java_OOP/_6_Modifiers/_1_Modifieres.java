26f19800c302dd7370e02c89398e2a57ef2e2ae1
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-23 17:42:47 +0600

    Java OOP [Modifieres] : public

---
+++ Java_W3School/_3_Java_OOP/_6_Modifiers/_1_Modifieres.java
@@ -0,0 +1,41 @@
+/*
+Java OOP [Modifieres] : public
+public : is an access modifier, meaning that it is use to set the access level for classes, attributes, methodes, and constructors.
+
+Access Modifiers:
+[class level access modifier]: 
+public: it can allow the class to access from any other class
+default: the class is accessible by the class in the same package
+
+[attribute, method, constructor level access modifiers]:
+public: the code is accessible for all classes
+private: the code is only accessible in the same package
+default: the code is only accessible in the same package
+protected: the code is accessible in the same package
+
+Non-Access Modifieres: 
+[class level Modifieres]:
+final: the class can not be inherited by other classes
+abstract: the class can not be used to create object
+
+final: attributes or method can not be override / modified
+static: attributes and method belongs to the class rather than an objects
+abstract: can only be used in abstract class, and can only be used on methods. The method does not have a body. For example, abstract void run();
+transient: Attributes and methods are skipped when serializing the object containing them
+synchronized: method can only be accessed by one thread at a time
+volatile: The value of an attribute is not cached thread-locally, and is always read from the "main memory"
+*/
+
+package Java_W3School._3_Java_OOP._6_Modifiers;
+
+public class _1_Modifieres {
+    final int x = 10;
+    final double PI = 3.14;
+
+    public static void main(String[] args) {
+        _1_Modifieres obj = new _1_Modifieres();
+        // obj.x = 100;    // generates an error: final value cannot be changed
+        // obj.PI = 2.90;    // generates an error: final value cannot be changed
+        System.out.println(obj.x);
+    }
+}
