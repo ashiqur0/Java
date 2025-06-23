0b3387549beb0ee86ea4226b655388267764f54f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-23 14:24:20 +0600

    OOP [Class Attribute] : Access Attribute

---
+++ Java_W3School/_3_Java_OOP/_2_Class_Attribute/_2_access_attribute.java
@@ -0,0 +1,13 @@
+/*
+OOP [Class Attribute] : Access Attribute
+*/
+
+package Java_W3School._3_Java_OOP._2_Class_Attribute;
+
+public class _2_access_attribute {
+    int num = 50;
+    public static void main(String[] args) {
+        _2_access_attribute obj = new _2_access_attribute();
+        System.out.println("Number is: " + obj.num);
+    }
+}
