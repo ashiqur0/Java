5fd922ff580efc1d4fea6e79dfed26c978206289
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-23 18:05:23 +0600

    Java OOP [Modifieres] : abstract

--- Java_W3School/_3_Java_OOP/_6_Modifiers/_4_abstract.java
+++ Java_W3School/_3_Java_OOP/_6_Modifiers/_4_abstract.java
@@ -1,10 +1,31 @@
 /*
-Java OOP [Modifieres] : a
-final: attributes or method can not be override / modified
+Java OOP [Modifieres] : abstract
+abstract: can only be used in abstract class, and can only be used on methods. The method does not have a body. For example, abstract void run();
 */
 
 package Java_W3School._3_Java_OOP._6_Modifiers;
 
+abstract class  Main {
+    public int roll = 20;
+    public String name = "Munna";
+    abstract void study();
+}
+
+class  Student extends Main {
+    public int graduation_year = 2027;
+    public void study() {
+        System.out.println("Study all day long...");
+    }    
+}
+
+
 public class _4_abstract {
-    
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.study();
+
+        System.out.println("Roll: " + student.roll);
+        System.out.println("Name: " + student.name);
+        System.out.println("Graduation Year: " + student.graduation_year);
+    }    
 }
