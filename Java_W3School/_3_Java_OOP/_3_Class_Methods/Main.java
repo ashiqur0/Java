e749009ca7f5f7c3cf37b97644a516f0c40b138d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-23 15:06:19 +0600

    OOP [Class Methods] : Multiple class

---
+++ Java_W3School/_3_Java_OOP/_3_Class_Methods/Main.java
@@ -0,0 +1,13 @@
+package Java_W3School._3_Java_OOP._3_Class_Methods;
+
+public class Main {
+
+    public void go() {
+        System.out.println("The car is running...");
+    }
+
+    public void max(int maxSpeed) {
+        System.out.println("The maxspeed: " + maxSpeed);
+    }
+
+}
---
+++ Java_W3School/_3_Java_OOP/_3_Class_Methods/_4_multiple_class.java
@@ -0,0 +1,12 @@
+/*
+OOP [Class Methods] : Multiple class
+*/
+
+package Java_W3School._3_Java_OOP._3_Class_Methods;
+public class _4_multiple_class {
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.go();
+        m.max(200);
+    }
+}
