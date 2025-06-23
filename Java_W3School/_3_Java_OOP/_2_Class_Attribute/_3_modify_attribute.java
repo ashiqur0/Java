027975e835304e02b69757b4ad5de6703cd8bf43
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-23 14:26:22 +0600

    OOP [Class Attribute] : Modify Attribute

---
+++ Java_W3School/_3_Java_OOP/_2_Class_Attribute/_3_modify_attribute.java
@@ -0,0 +1,14 @@
+/*
+OOP [Class Attribute] : Modify Attribute
+*/
+
+package Java_W3School._3_Java_OOP._2_Class_Attribute;
+
+public class _3_modify_attribute {
+    int x = 2;
+    public static void main(String[] args) {
+        _3_modify_attribute obj = new _3_modify_attribute();
+        obj.x = 5;
+        System.out.println(obj.x);  // 5 
+    }
+}
