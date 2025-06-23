f32c77a10cf9963da706615fc8107cd0acd06044
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-23 14:08:54 +0600

    Java OOP : Create Class and Object

---
+++ Java_W3School/_3_Java_OOP/_1_Class_Object/_1_class_object.java
@@ -0,0 +1,12 @@
+/*
+Java OOP : Create Class and Object
+*/
+package Java_W3School._3_Java_OOP._1_Class_Object;
+
+public class _1_class_object {
+    int x = 5;
+    public static void main(String[] args) {
+        _1_class_object obj = new _1_class_object();
+        System.out.println(obj.x);
+    }
+}
