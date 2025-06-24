47a2dddba43240245b68ed0ad0ea35b72dd0bd1e
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-24 19:54:21 +0600

    Java Enums: group of constant

--- Java_W3School/_3_Java_OOP/_15_Enums/_1_enum.java
+++ Java_W3School/_3_Java_OOP/_15_Enums/_1_enum.java
@@ -1,6 +1,14 @@
 /*
-Java OOP : Enums
+Java Enums: group of constant
 enum : is a special class that reperesent a group of constant (unchangable variable like final variables)
+
+Difference between Enums and Classes
+An enum can, just like a class, have attributes and methods. The only difference is that enum constants are public, static and final (unchangeable - cannot be overridden).
+
+An enum cannot be used to create objects, and it cannot extend other classes (but it can implement interfaces).
+
+Why And When To Use Enums?
+Use enums when you have values that you know aren't going to change, like month days, days, colors, deck of cards, etc.
 
 */
 
