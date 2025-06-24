a7bf25f65727f7b7d82c2ccd349c7dc4b945d641
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-24 13:48:17 +0600

    Java OOP : Encapsulation

--- Java_W3School/_3_Java_OOP/_7_encapsulation/_1_Encapsulation.java
+++ Java_W3School/_3_Java_OOP/_7_encapsulation/_1_Encapsulation.java
@@ -5,6 +5,12 @@ Encapsulation: The meaning of Encapsulation, is to make sure that "sensitive" da
 To achieve this, you must:
 declare class variables/attributes as private
 provide public get and set methods to access and update the value of a private variable
+
+Why Encapsulation?
+Better control of class attributes and methods
+Class attributes can be made read-only (if you only use the get method), or write-only (if you only use the set method)
+Flexible: the programmer can change one part of the code without affecting other parts
+Increased security of data
 */
 
 package Java_W3School._3_Java_OOP._7_encapsulation;
