1e5fcd644e039f73e2a17376d26208a38a01e93c
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-24 14:53:51 +0600

    Java OOP : Inheritance [final class]

---
+++ Java_W3School/_3_Java_OOP/_9_inheritance/_2_final_class.java
@@ -0,0 +1,15 @@
+/*
+Java OOP : Inheritance [final class]
+final : final class cannot be inheritad
+*/
+
+// package Java_W3School._3_Java_OOP._9_inheritance;
+// final class Bird {
+
+// }
+
+// public class _2_final_class extends Bird {  // error : final class cannot be inheritad
+//     public static void main(String[] args) {
+        
+//     }
+// }
