52cceba111c44313e92b892418043583d5b1c729
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-24 19:40:10 +0600

    Java OOP : Enums: enum inside a class

--- Java_W3School/_3_Java_OOP/_15_Enums/_2_enum_inside_a_class.java
+++ Java_W3School/_3_Java_OOP/_15_Enums/_2_enum_inside_a_class.java
@@ -1,5 +1,21 @@
+/*
+Java OOP : Enums: enum inside a class
+enum : is a special class that reperesent a group of constant (unchangable variable like final variables)
+*/
+
 package Java_W3School._3_Java_OOP._15_Enums;
 
+class Main {
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+}
+
 public class _2_enum_inside_a_class {
-    
+    public static void main(String[] args) {
+        Level level = Level.MEDIUM;
+        System.out.println(level);
+    }
 }
