3ed6ad79dc5813b3451293494dc98387767479b7
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-24 18:32:53 +0600

    Java OOP : Abstraction

---
+++ Java_W3School/_3_Java_OOP/_13_Abstruction/_1_abstruct.java
@@ -0,0 +1,33 @@
+/*
+Java OOP : Abstraction
+Abstract classes and methods: Data abstraction is the process of hiding certain details and showing only essential information to the user.
+Abstraction can be achieved with either abstract classes or interfaces
+
+Abstract class: is a restricted class that cannot be used to create objects (to access it, it must be inherited from another class).
+
+Abstract method: can only be used in an abstract class, and it does not have a body. The body is provided by the subclass (inherited from).
+*/
+
+package Java_W3School._3_Java_OOP._13_Abstruction;
+
+abstract class Animal {         // abstract class, cannot create object
+    abstract void sound();      // abstract method sound(); without body
+
+    public void sleep() {       // can also have normal method
+        System.out.println("Zzz..");
+    }
+}
+
+class Dog extends Animal {
+    public void sound() {
+        System.out.println("bow bow...");
+    }
+}
+
+public class _1_abstruct {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.sound();
+        dog.sleep();
+    }
+}
