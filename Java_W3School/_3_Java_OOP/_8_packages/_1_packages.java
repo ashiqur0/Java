6ca5141346d2ff31d87530b315c3d3ff6c2ef10e
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-24 14:28:09 +0600

    Java OOP : Packages

--- Java_W3School/_3_Java_OOP/_8_packages/_1_packages.java
+++ Java_W3School/_3_Java_OOP/_8_packages/_1_packages.java
@@ -10,8 +10,8 @@ import package.name.Class;   // Import a single class
 import package.name.*;       // Import the whole package
 */
 
-package Java_W3School._3_Java_OOP._8_packages;
-import java.util.Scanner;
+package Java_W3School._3_Java_OOP._8_packages;  // এটাই ইউজার ডিফাইন প্যাকেজ
+import java.util.Scanner;                       // এটা Built-in Packages
 public class _1_packages {
     public static void main(String[] args) {
         @SuppressWarnings("resource")
