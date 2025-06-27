c29859a1d85e6478946f25c90589c1a33f2ad968
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-27 22:10:26 +0600

    Java Exceptions : What is exceptions?

---
+++ Java_W3School/_4_Java_Error/_3_Exceptons/_1_Exceptions.java
@@ -0,0 +1,15 @@
+/*
+Java Exceptions : What is exceptions?
+different types of errors can occurs while running a program. Such as, such as coding mistakes, invalid input, or unexpected situations.
+
+while error occurs, Java will throws an exception (throw error)
+*/
+
+
+package Java_W3School._4_Java_Error._3_Exceptons;
+
+public class _1_Exceptions {
+    public static void main(String[] args) {
+        
+    }
+}
