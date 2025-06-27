5bd7d8b2a0a9a02f4f6de488af81dad429bc8f3d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-27 20:47:28 +0600

    Java Errors : Compile Time Error

---
+++ Java_W3School/_4_Java_Error/_1_Error/_1_error.java
@@ -0,0 +1,21 @@
+/*
+Java Errors : Compile Time Error
+
+Compile Time Error : Compile-time errors occur when the program cannot compile due to syntax or type issues.
+    1. Missing Semicolon
+    2. Undeclared Variables
+    3. Mismatched Types
+*/
+
+package Java_W3School._4_Java_Error._1_Error;
+
+public class _1_error {
+    public static void main(String[] args) {
+    //     int x = 10                  // Error : Missing Semicolon
+    //     System.out.println(x);
+
+    //     System.out.println(myVer);  // Error : Undeclared Variables
+
+    //     int y = "hello";            // Error : Mismatched Types
+    }    
+}
