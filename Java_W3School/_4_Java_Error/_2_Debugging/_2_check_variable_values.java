27a08d5648c46b976fcfbc997b4c481140d8f3c0
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-27 21:57:46 +0600

    Java Debugginh : Check Variable Values

--- Java_W3School/_4_Java_Error/_2_Debugging/_2_check_variable_values.java
+++ Java_W3School/_4_Java_Error/_2_Debugging/_2_check_variable_values.java
@@ -1,6 +1,16 @@
 /*
 Java Debugginh : Check Variable Values
 If something unexpected happens, print out the values of your variables:
+
+Debugging Checklist
+Read the full error message, it often tells you exactly what's wrong
+Check if all variables are initialized before use
+Print variable values to trace the problem
+Watch for off-by-one errors in loops and arrays
+Comment out sections of code to find bugs
+
+Debugging with IDEs
+Modern IDEs like IntelliJ IDEA, Eclipse, and NetBeans come with built-in debugging tools.
 */
 
 package Java_W3School._4_Java_Error._2_Debugging;
