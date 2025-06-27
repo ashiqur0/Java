7b90423b6e2f4e149d443c0100897a15a313138d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-27 21:35:35 +0600

    Java Errors : Logical Error

---
+++ Java_W3School/_4_Java_Error/_1_Error/_3_Logical_Error.java
@@ -0,0 +1,19 @@
+/*
+Java Errors : Logical Error
+Example : mistakenly subtract instead of sum
+*/
+
+package Java_W3School._4_Java_Error._1_Error;
+
+public class _3_Logical_Error {
+    public static void main(String[] args) {
+        
+        // logical error : mistakenly subtract instead of sum
+        int x = 10;
+        int y = 2;
+        int sum = x - y;
+        System.out.println("sum: " + sum);
+
+
+    }
+}
