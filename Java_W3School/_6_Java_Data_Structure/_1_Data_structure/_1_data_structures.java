232f8f9ea7e417556227d5496f8a5e3f57fbb3f4
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-28 15:23:44 +0600

    Java Data Structure

---
+++ Java_W3School/_6_Java_Data_Structure/_1_Data_structure/_1_data_structures.java
@@ -0,0 +1,17 @@
+/*
+Java Data Structure : 
+Data Structure: is a way to store and organize data.
+Ex. Arrays. which allows to stoe many similar type data to store in a single variable .
+
+Java includes many other Data Structure
+ArrayList
+HashSet
+HashMap
+
+*/
+
+package Java_W3School._6_Java_Data_Structure._1_Data_structure;
+
+public class _1_data_structures {
+    
+}
