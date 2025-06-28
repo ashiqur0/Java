061ac6cbc0b90b76066ae79fdfa7cdba5c242d3f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-28 23:56:32 +0600

    Java Data Structure : ArrayList

---
+++ Java_W3School/_6_Java_Data_Structure/_3_ArrayLIst/_1_create_ArrayList.java
@@ -0,0 +1,16 @@
+/*
+Java Data Structure : ArrayList
+ArrayList: is like a resizable array.
+
+*/
+
+package Java_W3School._6_Java_Data_Structure._3_ArrayLIst;
+
+import java.util.ArrayList;                             // import ArrayList class
+
+public class _1_create_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();   // create an object of ArrayList
+        System.out.println(fruits);
+    }
+}
