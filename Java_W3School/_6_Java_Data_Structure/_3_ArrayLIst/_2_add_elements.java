ab989d94c8ea79d3393c61e38e43c80487cae56e
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-28 23:59:21 +0600

    Java Data Structure : Add elements to the ArrayList

--- Java_W3School/_6_Java_Data_Structure/_3_ArrayLIst/_2_add_elements.java
+++ Java_W3School/_6_Java_Data_Structure/_3_ArrayLIst/_2_add_elements.java
@@ -1,5 +1,18 @@
+/*
+Java Data Structure : Add elements to the ArrayList
+
+*/
+
 package Java_W3School._6_Java_Data_Structure._3_ArrayLIst;
 
+import java.util.ArrayList;
+
 public class _2_add_elements {
-    
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("cherry");
+        fruits.add("kiwi");
+    }
 }
