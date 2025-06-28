f21fb9c5f8d2a58100dad48b5167bd1b5ff84c49
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-28 20:26:55 +0600

    Java Data Structure : The Collection Framework

---
+++ Java_W3School/_6_Java_Data_Structure/_2_Java_collections/_1_collection_framework.java
@@ -0,0 +1,36 @@
+/*
+Java Data Structure : The Collection Framework
+The Collection Framework allows  add, remove, sort, and search elements easily.
+
+Core interfaces in the collection framework:
+Core Interfaces     Common Classes                         Description
+    List            ArrayList, LinkedList                  ordered collection, allow duplicates
+    Set             HashSet, TreeSet, LinkedHashSet        collection of unique elements
+    Map             HashMap, TreeMap, LinkedHashMap        store key-value pairs with unique keys
+
+Overview of Classes:
+Interface       Class           Description
+List            ArrayList       Resizable array, maintain order, allow duplicate
+                LinkedList      List with fast insert and remove operations
+
+Set             Hashset         Unordered collection of unique elements
+                TreeSet         Sorted set of unique elements (natural order)
+                LinkedHashSet   Maintain the order in which element is inserted
+
+Map             HashMap         Stores key-value pairs with no specific order
+                TreeMap         Sorted map based on the natural order of keys
+                LinkedHashMap   Maintain the order in which key were inserted
+
+When use?
+List    when care about ordered, want to store duplicates, want to access through indexed
+Set     when need to unique value
+Map     when want to store key-value pair
+*/
+
+package Java_W3School._6_Java_Data_Structure._2_Java_collections;
+
+public class _1_collection_framework {
+    public static void main(String[] args) {
+        
+    }
+}
