3018939159e9d8f0d1912b1cb410cb811932e516
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-29 00:41:46 +0600

    Java Data Structure : Different type of ArrayList

---
+++ Java_W3School/_6_Java_Data_Structure/_3_ArrayLIst/_10_different_type_ArrayList.java
@@ -0,0 +1,26 @@
+/*
+Java Data Structure : Different type of ArrayList
+
+*/
+
+package Java_W3School._6_Java_Data_Structure._3_ArrayLIst;
+
+import java.util.ArrayList;
+
+public class _10_different_type_ArrayList {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        ArrayList<Byte> nums = new ArrayList<>(); 
+        ArrayList<Short> nums2 = new ArrayList<>(); 
+        ArrayList<Integer> nums3 = new ArrayList<>(); 
+        ArrayList<Long> nums4 = new ArrayList<>(); 
+
+        ArrayList<Float> price = new ArrayList<>();
+        ArrayList<Double> price2 = new ArrayList<>();
+
+        ArrayList<Character> ch = new ArrayList<>();
+        ArrayList<Boolean> bool = new ArrayList<>();
+
+        ArrayList<String> str = new ArrayList<>();
+    }
+}
