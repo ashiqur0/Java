88c0f865213ef59c13f08cdda056184655781938
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-28 19:54:22 +0600

    Java Data Structure : Iterator

---
+++ Java_W3School/_6_Java_Data_Structure/_1_Data_structure/_5_Iterator.java
@@ -0,0 +1,28 @@
+/*
+Java Data Structure : Iterator
+*/
+
+package Java_W3School._6_Java_Data_Structure._1_Data_structure;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+class _5_Iterator {
+    public static void main(String[] args) {
+
+        // get the object of Integers
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        nums.add(20);
+        nums.add(30);
+        nums.add(40);
+        nums.add(50);
+
+        // get the iterator for the ArrayList
+        Iterator<Integer> it = nums.iterator();
+
+        // iterate through the list using iterator
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
