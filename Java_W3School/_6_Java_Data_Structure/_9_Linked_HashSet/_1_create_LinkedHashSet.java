59bc4d902130a9c0506c02f89ae73f657bb25181
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-30 08:11:36 +0600

    LinkedHashSet : Create LinkedHashSet

---
+++ Java_W3School/_6_Java_Data_Structure/_9_Linked_HashSet/_1_create_LinkedHashSet.java
@@ -0,0 +1,15 @@
+/*
+LinkedHashSet : Create LinkedHashSet
+LinkedHashSet is a collection of unique elements, maintain insertin sequence.
+*/
+
+package Java_W3School._6_Java_Data_Structure._9_Linked_HashSet;
+
+import java.util.LinkedHashSet;
+
+public class _1_create_LinkedHashSet {
+    public static void main(String[] args) {
+        LinkedHashSet<Integer> nums = new LinkedHashSet<>();
+        System.out.println(nums);
+    }
+}
