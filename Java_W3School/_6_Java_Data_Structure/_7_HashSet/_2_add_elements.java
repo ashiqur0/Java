f8c37d11d7c18b8f40a811554a745a565a6c55d3
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-30 07:15:49 +0600

    Set : Add Elements to the HashSet

---
+++ Java_W3School/_6_Java_Data_Structure/_7_HashSet/_2_add_elements.java
@@ -0,0 +1,18 @@
+/*
+Set : Add Elements to the HashSet
+*/
+
+package Java_W3School._6_Java_Data_Structure._7_HashSet;
+
+import java.util.HashSet;
+
+public class _2_add_elements {
+    public static void main(String[] args) {
+        HashSet<Integer> nums = new HashSet<>();
+        nums.add(20);
+        nums.add(25);
+        nums.add(30);
+
+        nums.forEach(n -> System.out.print(n + " "));
+    }
+}
