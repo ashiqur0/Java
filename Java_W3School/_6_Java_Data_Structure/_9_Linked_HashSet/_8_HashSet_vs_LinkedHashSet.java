12961a20d145c09c92a947f58e985c92c84013d1
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-30 08:38:48 +0600

    LinkedHashSet : HashSet vs LinkedHashSet

--- Java_W3School/_6_Java_Data_Structure/_9_Linked_HashSet/_8_HashSet_vs_LinkedHashSet.java
+++ Java_W3School/_6_Java_Data_Structure/_9_Linked_HashSet/_8_HashSet_vs_LinkedHashSet.java
@@ -6,6 +6,7 @@ no guaranteed order         maintain insertion order
 duplicate not allowed       duplicate not allowed
 faster                      slightly slower(due to maintain sorting)
 
+Tip: Use HashSet when you only care about uniqueness and speed. Use LinkedHashSet when order matters.
 */
 
 package Java_W3School._6_Java_Data_Structure._9_Linked_HashSet;
