c371c360938f371648ea5eb4262ad4469936dc7c
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-30 07:03:56 +0600

    Set : Set Interface

---
+++ Java_W3School/_6_Java_Data_Structure/_6_Set/_1_Set_Interface.java
@@ -0,0 +1,29 @@
+/*
+Set : Set Interface
+Set is used to store unique elements.
+
+3 Classes that implements Set
+HashSet
+TreeSet
+LinkedHashSet
+
+Common set methods: 
+add(), 
+remove(), 
+contains(), 
+size(), 
+clear()
+
+List          vs    Set
+Allow duplicates    Does not allow duplicates
+Maintian order      Does not gurantee order
+Access by index     Access by key
+*/
+
+package Java_W3School._6_Java_Data_Structure._6_Set;
+
+public class _1_Set_Interface {
+    public static void main(String[] args) {
+        
+    }
+}
