41364ab0f2422878cec9b72ed5e818f04f48a5ae
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-30 07:44:47 +0600

    TreeSet : Create TreeSet

---
+++ Java_W3School/_6_Java_Data_Structure/_8_TreeSet/_1_create_TreeSet.java
@@ -0,0 +1,15 @@
+/*
+TreeSet : Create TreeSet
+TreeSet is a collection that stores unique elements in sorted order.
+*/
+
+package Java_W3School._6_Java_Data_Structure._8_TreeSet;
+
+import java.util.TreeSet;
+
+public class _1_create_TreeSet {
+    public static void main(String[] args) {
+        TreeSet<Integer> nums = new TreeSet<>();
+        System.out.println(nums);
+    }
+}
