c5c53a52a141b380e71be9cea8a5f3024805cfe8
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-30 07:12:14 +0600

    Set : Create HashSet

---
+++ Java_W3School/_6_Java_Data_Structure/_7_HashSet/_1_create_HashSet.java
@@ -0,0 +1,15 @@
+/*
+Set : Create HashSet
+HashSet is a collection of unique elements
+*/
+
+package Java_W3School._6_Java_Data_Structure._7_HashSet;
+
+import java.util.HashSet;
+
+public class _1_create_HashSet {
+    public static void main(String[] args) {
+        HashSet<String> car = new HashSet<>();
+        System.out.println(car);
+    }
+}
