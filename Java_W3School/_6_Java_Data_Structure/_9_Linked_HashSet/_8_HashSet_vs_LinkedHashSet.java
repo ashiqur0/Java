6ae2f65380dc51bc1db00d3da5c6ac59431c9074
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-30 08:37:44 +0600

    LinkedHashSet : HashSet vs LinkedHashSet

---
+++ Java_W3School/_6_Java_Data_Structure/_9_Linked_HashSet/_8_HashSet_vs_LinkedHashSet.java
@@ -0,0 +1,15 @@
+/*
+LinkedHashSet : HashSet vs LinkedHashSet
+
+HashSet         vs          LinkedHashSet
+no guaranteed order         maintain insertion order
+duplicate not allowed       duplicate not allowed
+faster                      slightly slower(due to maintain sorting)
+
+*/
+
+package Java_W3School._6_Java_Data_Structure._9_Linked_HashSet;
+
+public class _8_HashSet_vs_LinkedHashSet {
+    
+}
