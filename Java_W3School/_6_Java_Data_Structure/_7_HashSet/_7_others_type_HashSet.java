0a196e1a152f034a9314f44571ee235c01ba5ee0
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-30 07:38:50 +0600

    HashSet : Others Type HashSet

---
+++ Java_W3School/_6_Java_Data_Structure/_7_HashSet/_7_others_type_HashSet.java
@@ -0,0 +1,25 @@
+/*
+HashSet : Others Type HashSet
+*/
+
+
+package Java_W3School._6_Java_Data_Structure._7_HashSet;
+
+import java.util.HashSet;
+
+public class _7_others_type_HashSet {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        HashSet<Byte> nums = new HashSet<>();
+        HashSet<Short> nums2 = new HashSet<>();
+        HashSet<Integer> nums3 = new HashSet<>();
+        HashSet<Long> nums4 = new HashSet<>();
+
+        HashSet<Float> f = new HashSet<>();
+        HashSet<Double> d = new HashSet<>();
+
+        HashSet<Boolean> bool = new HashSet<>();
+        HashSet<Character> ch = new HashSet<>();
+        HashSet<String> str = new HashSet<>();        
+    }
+}
