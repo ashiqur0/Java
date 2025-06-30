cdcd0517b8c3f3e33670118c49b0f58aa27195be
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-30 08:06:09 +0600

    TreeSet : HashSet vs TreeSet

---
+++ Java_W3School/_6_Java_Data_Structure/_8_TreeSet/_7_HashSet_vs_TreeSet.java
@@ -0,0 +1,14 @@
+/*
+TreeSet : HashSet vs TreeSet
+
+HashSet     vs      TreeSet
+unordered           ordered(natural order)
+no duplicates       no duplicates
+faster(no sorting)  slower(due to sorting)
+*/
+
+package Java_W3School._6_Java_Data_Structure._8_TreeSet;
+
+public class _7_HashSet_vs_TreeSet {
+    
+}
