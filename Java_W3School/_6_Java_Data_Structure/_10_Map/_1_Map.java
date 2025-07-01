0dc2122213c310a919f71fc6dc1a19645b63b57d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-01 10:09:42 +0600

    DSA : LinearSearch

---
+++ Java_W3School/_6_Java_Data_Structure/_10_Map/_1_Map.java
@@ -0,0 +1,25 @@
+/*
+Map : About Map Interface
+
+Map: 
+Stores key-value pair, 
+Each key must be unique,
+Value can be duplicate
+
+Common Classes that Implement Map
+HashMap - fast and unordered
+TreeMap - sorted by key
+LinkedHashMap - sorted by insertion order
+
+Common Map methods
+
+*/
+
+package Java_W3School._6_Java_Data_Structure._10_Map;
+
+public class _1_Map {
+    public static void main(String[] args) {
+        var a = 10;
+        System.out.println(a);
+    }
+}
