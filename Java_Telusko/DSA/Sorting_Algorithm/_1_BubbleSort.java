15728968514ceef65cedb6e1bf65584933ab6076
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-01 15:50:54 +0600

    DSA : Bubble Sort

--- Java_Telusko/DSA/Sorting_Algorithm/_1_BubbleSort.java
+++ Java_Telusko/DSA/Sorting_Algorithm/_1_BubbleSort.java
@@ -1,10 +1,9 @@
 /*
 DSA : Bubble Sort
-
 */
 
 package Java_Telusko.DSA.Sorting_Algorithm;
-public class Inner_1_BubbleSort {
+public class _1_BubbleSort {
     public static void main(String[] args) {
         int [] arr = {2, 3, 4, 5, 1, 0};
         for (int i = 0; i < arr.length; i++) {
