eba8e98a9f0b0e785428ab81ef51bc48eb4ecdeb
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-02 21:59:25 +0600

    Hash Map : Different Types of TreeMap

---
+++ Java_W3School/_6_Java_Data_Structure/_12_TreeMap/_10_others_type_TreeMap.java
@@ -0,0 +1,18 @@
+/*
+Hash Map : Different Types of TreeMap
+
+TreeMap: stores key-value pair, in a sorted order by key
+*/
+
+package Java_W3School._6_Java_Data_Structure._12_TreeMap;
+
+import java.util.TreeMap;
+
+public class _10_others_type_TreeMap {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        TreeMap<String, Double> itemsPrice = new TreeMap<>();
+        TreeMap<String, Boolean> result = new TreeMap<>();
+        TreeMap<String, String> capitalCity = new TreeMap<>();
+    }
+}
