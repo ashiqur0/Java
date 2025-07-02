c9b3b189680d3f65e7ec8008333358ab212db2b6
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-02 19:41:12 +0600

    Map : Java Map Interface

--- Java_W3School/_6_Java_Data_Structure/_10_Map/_1_Map.java
+++ Java_W3School/_6_Java_Data_Structure/_10_Map/_1_Map.java
@@ -1,5 +1,5 @@
 /*
-Map : About Map Interface
+Map : Java Map Interface
 
 Map: 
 Stores key-value pair, 
@@ -12,14 +12,22 @@ TreeMap - sorted by key
 LinkedHashMap - sorted by insertion order
 
 Common Map methods
+put(): stores or updates key-value pairs
+get(): retrieve value using key 
+remove(): remove the key and its value
+containsKey(): check if the map contains the key
+keySet(): Returns a set of all keys
 
+Map vs Set vs List
+Features                    List    Set                                    Map
+Duplicate allowed           Yes     No                                     Key - NO, Value - Yes
+Stores key-value pairs?     No      No                                     Yes
+Maintain Order?             Yes     NO(except TreeSet or LinkedHashSet)    No(except TreeMap or LinkedHashMap)
 */
-
 package Java_W3School._6_Java_Data_Structure._10_Map;
 
 public class _1_Map {
     public static void main(String[] args) {
-        var a = 10;
-        System.out.println(a);
+        
     }
 }
