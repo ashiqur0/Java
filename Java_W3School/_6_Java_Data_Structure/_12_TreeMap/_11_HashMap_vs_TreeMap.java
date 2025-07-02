59db22b75e804366046da485e3260a0be4f159fe
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-02 22:04:01 +0600

    Hash Map : HashMap vs TreeMap

---
+++ Java_W3School/_6_Java_Data_Structure/_12_TreeMap/_11_HashMap_vs_TreeMap.java
@@ -0,0 +1,14 @@
+/*
+Hash Map : HashMap vs TreeMap
+
+Feature                    HashMap                         TreeMap
+Order                      No guaranteed order             Sorted by key
+performance                Faster (no sorting)             Slower (maintain sorted order)
+*/
+
+
+package Java_W3School._6_Java_Data_Structure._12_TreeMap;
+
+public class _11_HashMap_vs_TreeMap {
+    
+}
