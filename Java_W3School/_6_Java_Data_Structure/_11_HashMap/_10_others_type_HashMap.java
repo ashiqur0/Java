96010bec395ba9a3776bbf3770937f411c798b04
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-02 20:36:13 +0600

    Hash Map : Others type of HashMap

---
+++ Java_W3School/_6_Java_Data_Structure/_11_HashMap/_10_others_type_HashMap.java
@@ -0,0 +1,19 @@
+/*
+Hash Map : Others type of HashMap
+
+HashMap: stores key-value pair, where each key map to specific values
+It can stores different types of combinations. Such as, String- Integer, String-String etc.
+*/
+
+package Java_W3School._6_Java_Data_Structure._11_HashMap;
+
+import java.util.HashMap;
+
+public class _10_others_type_HashMap {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        HashMap<Integer, Boolean> passStudents = new HashMap<>();
+        HashMap<String, Double> productPrice = new HashMap<>();
+        HashMap<String, Integer> score = new HashMap<>();
+    }
+}
