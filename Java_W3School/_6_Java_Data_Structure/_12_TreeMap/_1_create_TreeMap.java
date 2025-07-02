28b4ec45313928dac155c69ca23e6d8c53f5b126
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-02 20:40:25 +0600

    Hash Map : Create TreeMap

---
+++ Java_W3School/_6_Java_Data_Structure/_12_TreeMap/_1_create_TreeMap.java
@@ -0,0 +1,17 @@
+/*
+Hash Map : Create TreeMap
+
+TreeMap: stores key-value pair, in a sorted order by key
+*/
+
+
+package Java_W3School._6_Java_Data_Structure._12_TreeMap;
+
+import java.util.TreeMap;
+
+public class _1_create_TreeMap {
+    public static void main(String[] args) {
+        TreeMap<Integer, String> studnets = new TreeMap<>();
+        System.out.println(studnets);
+    }
+}
