d1a370077a63ee57b0e9b138696616ce1f3414f5
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-02 19:54:31 +0600

    Hash Map : Add Items to the HashMap

---
+++ Java_W3School/_6_Java_Data_Structure/_11_HashMap/_2_add_Items.java
@@ -0,0 +1,21 @@
+/*
+Hash Map : Add Items to the HashMap
+
+HashMap: Add Items to the HashMap
+*/
+
+package Java_W3School._6_Java_Data_Structure._11_HashMap;
+
+import java.util.HashMap;
+
+public class _2_add_Items {
+    public static void main(String[] args) {
+        HashMap<Integer, String> students = new HashMap<>();
+        students.put(1, "Samiul");
+        students.put(2, "Nawshin");
+        students.put(3, "Simanto");
+        students.put(4, "Ruhi");
+
+        System.out.println(students);
+    }
+}
