2c4239237e7b5b7be89733b187b72157cd0c847d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-02 19:50:56 +0600

    Map : Hash Map

---
+++ Java_W3School/_6_Java_Data_Structure/_11_HashMap/_1_create_HashMap.java
@@ -0,0 +1,18 @@
+/*
+Map : Hash Map
+
+HashMap: stores key-value pair, where each key map to specific values
+It can stores different types of combinations. Such as, String- Integer, String-String etc.
+*/
+
+package Java_W3School._6_Java_Data_Structure._11_HashMap;
+
+import java.util.HashMap;                                  // Import HashMap class
+
+public class _1_create_HashMap {
+    public static void main(String[] args) {
+        HashMap<Integer, String> students = new HashMap<>();// create HashMap obj
+        
+        System.out.println(students);
+    }
+}
