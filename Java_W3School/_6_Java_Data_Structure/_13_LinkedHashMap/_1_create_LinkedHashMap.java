dc2e25971e2fbbd565b4773ea710fb71ae8eb212
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-03 22:04:18 +0600

    Hash Map : Create LinkedMap

---
+++ Java_W3School/_6_Java_Data_Structure/_13_LinkedHashMap/_1_create_LinkedHashMap.java
@@ -0,0 +1,17 @@
+/*
+Hash Map : Create LinkedMap
+
+TreeMap: stores key-value pair, maintain insertion order
+*/
+
+package Java_W3School._6_Java_Data_Structure._13_LinkedHashMap;
+
+import java.util.LinkedHashMap;
+
+public class _1_create_LinkedHashMap {
+    public static void main(String[] args) {
+        LinkedHashMap<Integer, String> students = new LinkedHashMap<>();
+        System.out.println(students);
+
+    }
+}
