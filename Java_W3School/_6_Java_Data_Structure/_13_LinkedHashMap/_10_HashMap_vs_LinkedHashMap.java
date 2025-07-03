9219ff691eab090a4e1d26f48c4e1724194d0517
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-03 23:16:38 +0600

    Hash Map : HashMap vs LinkedHashMap

---
+++ Java_W3School/_6_Java_Data_Structure/_13_LinkedHashMap/_10_HashMap_vs_LinkedHashMap.java
@@ -0,0 +1,18 @@
+/*
+Hash Map : HashMap vs LinkedHashMap
+
+Features        HashMap                 LinkedHashMap
+Order           No guaranteed order     Insertion order preserved
+Performance     Faster random access    Slightly slower due to ordering
+Duplicates      Kye must be unique      key must be unique
+
+
+*/
+
+package Java_W3School._6_Java_Data_Structure._13_LinkedHashMap;
+
+public class _10_HashMap_vs_LinkedHashMap {
+    public static void main(String[] args) {
+        
+    }
+}
